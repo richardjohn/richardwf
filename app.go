0d6c9039df45fd923d752f55cbdc56d251c4f437
Richard John <richard@dominorecordco.com>
2018-02-24 16:12:46 +0000

    Initial commit

---
+++ app.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "log"
+  "net/http"
+  "google.golang.org/appengine"  
+)
+
+func main() {
+  fs := http.FileServer(http.Dir("static"))
+  http.Handle("/", fs)
+
+  log.Println("Listening...")
+  http.ListenAndServe(":80", nil)
+  appengine.Main()
+}
